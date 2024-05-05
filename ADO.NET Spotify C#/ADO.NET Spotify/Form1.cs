927823ffc7406d4d1bd1ea5fefc2cd2e861bf87d
unknown <tu4pmkvys@code.edu.az>
2024-05-05 23:40:14 +0400

    tapsiriq

---
+++ ADO.NET Spotify C#/ADO.NET Spotify/Form1.cs
@@ -0,0 +1,90 @@
+﻿
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ADO.NET_Spotify
+{
+    public partial class Form1 : Form
+    {
+        const string connectionString = "Server=DESKTOP-8ABCEA9\\SQLEXPRESS;Database=ADONETSpotify;Trusted_Connect;";
+        public Form1()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users", conn);
+            SqlDataReader datas = cmd.ExecuteReader();
+            DataGridView.Columns.Add();
+            while (datas.Read())
+            {
+                dataGridView1.Rows.Add(datas[0], datas[1], (datas[2], (datas[3]);
+            }
+
+            InitializeComponent();
+        }
+        void GetDatas()
+        {
+            ResetTable();
+            using SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            using SqlCommand command = new SqlCommand("SELECT * FROM People", conn);
+            using SqlDataReader datas = command.ExecuteReader();
+            while (datas.Read())
+            {
+                dataGridView1.Rows.Add(datas[0], datas[1], datas[2], (Convert.ToInt32(datas[3]) == 1 ? "Kişi" : "Qadın"));
+            }
+        }
+        bool InsertData(string name, string surname, int genderId)
+        {
+            if (name.Length < 3 || surname.Length < 3 || genderId < 1 || genderId > 2)
+            {
+                MessageBox.Show("Daxil olunan dəyərlərdə yalnışlıq var.");
+                return false;
+            }
+            using SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            using SqlCommand command = new SqlCommand($"INSERT INTO People VALUES (N'{name}',N'{surname}',{genderId})", conn);
+            int count = command.ExecuteNonQuery();
+            if (count > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            var data = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex];
+            string id = data.Cells[0].Value.ToString();
+            Delete(id);
+            GetDatas();
+        }
+        void Delete(string id)
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            using SqlCommand command = new SqlCommand($"DELETE People WHERE Id = {id}", conn);
+            var result = command.ExecuteNonQuery();
+            if (result > 0)
+            {
+                MessageBox.Show("Silindi");
+            }
+            else
+            {
+                MessageBox.Show("Gözlənilməz xəta baş verdi");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+     
+    }
+}
---
+++ ADO.NET Spotify C#/ADO.NET Spotify/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ADO.NET_Spotify
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var data = SqlHelper.ExecuteQuery($"SELECT Id FROM Users WHERE Username='{username.Text}' and Password='{SqlHelper.HashPassword(password.Text)}'   ");
+            if (data.Rows.Count > 0)
+            {
+                MessageBox.Show("Xoş gəldiniz");
+            }
+            else
+            {
+                MessageBox.Show("İstifadəçi adı və ya parolu yalnışdır.");
+            }
+
+        }
+
+
+    }
+}
